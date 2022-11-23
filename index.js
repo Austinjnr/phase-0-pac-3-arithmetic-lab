f577b14ad08d5af1db8c274e707c9f7c10a2792b
austinjnr <austinobonyo@gmail.com>
2022-11-23 07:15:53 +0300

    assignment completed

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(){
+
+}
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide(){
+
+}
+function add (n1, n2){
+    return n1 + n2;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(c, d){
+    return c * d;
+}
+function divide(a, b){
+    return a / b;
+}
+function increment(n){
+    return n += 1;
+}
+function decrement(n){
+    return n -= 1
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
